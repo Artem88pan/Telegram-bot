d8d3f73389e4946a9b0a8a26ae9330d23c5eb811
artem <vonap88@mail.ru>
2023-05-22 19:16:02 +0300

    add new file parsing weather

---
+++ requestBot.py
@@ -0,0 +1,10 @@
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '5876951218:AAHjecSKkQPJdF-dW5VUGIsQXE3i1C03dBk'
+
+offset: int = -2
+timeout: int = 50
+update: dict
